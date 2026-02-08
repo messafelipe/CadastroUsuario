6feb5a81277c8504aa953e3a832431a16b914a02
Felipe Messa <51674627+messafelipe@users.noreply.github.com>
2026-02-08 06:55:31 -0300

    feat: Alteração Program.cs

--- CadastroUsuario.API/Program.cs
+++ CadastroUsuario.API/Program.cs
@@ -1,8 +1,11 @@
 using CadastroUsuario.Application.Services;
 using CadastroUsuario.Application.Services.Interfaces;
+using CadastroUsuario.Core.Repositories.Interfaces;
 using CadastroUsuario.Core.Security.Interfaces;
 using CadastroUsuario.Infrastructure.Persistence;
+using CadastroUsuario.Infrastructure.Persistence.Repositories;
 using CadastroUsuario.Infrastructure.Security;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +14,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-builder.Services.AddDbContext<CadastroUsuarioDbContext>();
+builder.Services.AddDbContext<CadastroUsuarioDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 builder.Services.AddScoped<ISenhaHasher, SenhaHasher>();
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
